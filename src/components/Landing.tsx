f238d79bbb46aa5dcab6c47b534b868e55f7aae2
shi632 <146401225+shi632@users.noreply.github.com>
2026-04-28 00:50:03 +0530

    Updated portfolio text and resume fix

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -15,15 +15,16 @@ const Landing = ({ children }: PropsWithChildren) => {
             </h1>
           </div>
           <div className="landing-info">
-            <h3>A Creative</h3>
-            <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Designer</div>
-              <div className="landing-h2-2">Developer</div>
+            <h3>I am a</h3>
+             <h2 className="landing-info-h2">
+              <div className="landing-h2-1">Java Developer</div>
+             <div className="landing-h2-2">Aspiring Full Stack</div>
             </h2>
-            <h2>
-              <div className="landing-h2-info">Developer</div>
-              <div className="landing-h2-info-1">Designer</div>
-            </h2>
+
+          <h2>
+            <div className="landing-h2-info">Problem Solver</div>
+            <div className="landing-h2-info-1">Continuous Learner</div>
+         </h2>
           </div>
         </div>
         {children}
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -62,8 +62,8 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
+            <span>Java Developer</span> <span>Aspiring Full Stack Developer</span>
+            <span> Java Developer </span> Aspiring Full Stack Developer <span>A Creative Designer</span>
           </Marquee>
         </div>
         <div
